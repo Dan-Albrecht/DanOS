a746b662df0c80833b2d095204385e7a882d1ea0
Dan Albrecht <Dan-Albrecht@users.noreply.github.com>
2024-01-15 23:36:21 -0800

    speed up iterate

--- iterateInternal.ps1
+++ iterateInternal.ps1
@@ -7,8 +7,13 @@ try {
     nasm.exe .\bootloaderStage2.asm -f bin -o .\bootloaderStage2.bin
     .\kernel\buildKernel.ps1
 
-    Remove-Item .\DanOS.vhd
-    New-VHD -Path .\DanOS.vhd -Fixed -SizeBytes 3MB
+    # Really hate PowerShell sometimes
+    if (![System.IO.File]::Exists("${PSScriptRoot}\empty.vhd")) {
+        # Creation is too slow, so just cache an empty one and use it
+        New-VHD -Path empty.vhd -Fixed -SizeBytes 3MB
+    }
+
+    Copy-Item -Force .\empty.vhd .\DanOS.vhd
 
     $stage1Bytes = Get-Content .\bootloaderStage1.bin -Raw -AsByteStream
     if ($stage1Bytes.Length -ne 512 ) { Write-Error 'Bootloader should be exactly 512 bytes' }
@@ -22,10 +27,13 @@ try {
 
     $osBytes = Get-Content .\DanOS.vhd -Raw -AsByteStream
     for ($x = 0; $x -lt $stage1Bytes.Length; $x++ ) { $osBytes[$x] = $stage1Bytes[$x] }
-    for ($x = 0; $x -lt $stage2Bytes.Length; $x++ ) { $osBytes[$x+512] = $stage2Bytes[$x] }
-    for ($x = 0; $x -lt $kernelBytes.Length; $x++ ) { $osBytes[$x+1024] = $kernelBytes[$x] }
+    for ($x = 0; $x -lt $stage2Bytes.Length; $x++ ) { $osBytes[$x + 512] = $stage2Bytes[$x] }
+    for ($x = 0; $x -lt $kernelBytes.Length; $x++ ) { $osBytes[$x + 1024] = $kernelBytes[$x] }
 
-    Set-Content -Path .\DanOS.vhd -Value $osBytes -AsByteStream
+    Write-Host "Writing $($osBytes.Length) bytes"
+    [System.IO.File]::WriteAllBytes("${PSScriptRoot}\DanOS.vhd", $osBytes)
+
+    Write-Host "VHD ready"
 }
 finally {
     $PSNativeCommandUseErrorActionPreference = $oldErrorState
