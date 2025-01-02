2a805e508f92899ffbce7183133ff800d5e77304
Dan Albrecht <Dan-Albrecht@users.noreply.github.com>
2025-01-01 18:38:43 -0800

    cargo supports env variables changing

--- kernel/build.rs
+++ kernel/build.rs
@@ -3,8 +3,7 @@ use std::path::Path;
 fn main() {
     let local_path = Path::new(env!("CARGO_MANIFEST_DIR"));
 
-    // Hack to force this always to rebuild as we might change the environment variable and no any of the files
-    println!("cargo:rerun-if-changed=BLAH");
+    println!("cargo:rerun-if-env-changed=KERNEL32_LOAD_TARGET");
 
     // The default is here only because we want to just run a compile without
     // needing to know what the correct value is.
