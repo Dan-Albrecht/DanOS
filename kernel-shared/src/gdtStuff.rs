94e8e4db2422b8fbb04f9bf9b76c5aee7dcf530a
Dan Albrecht <Dan-Albrecht@users.noreply.github.com>
2025-02-16 01:02:50 -0800

    Start cleaning up GDT code; add code to switch to Unreal; clarify tooling for shared

--- kernel-shared/src/gdtStuff.rs
+++ kernel-shared/src/gdtStuff.rs
@@ -4,10 +4,112 @@ use core::arch::asm;
 
 use crate::memoryHelpers::{alignDown, zeroMemory2};
 use crate::{haltLoopWithMessage, vgaWriteLine};
-
 use crate::assemblyStuff::halt::haltLoop;
 
 const GDT_ALIGNMENT : usize = 0x10;
+
+// 3.5.1 Segment Descriptor Tables
+// Global Descriptor Table
+#[repr(C, align(0x8))]
+#[cfg(target_pointer_width = "32")]
+pub struct Gdt {
+    nullSection: u64,
+    codeSection: u64,
+    dataSection: u64,
+}
+
+// 2.4.1 Global Descriptor Table Register (GDTR)
+#[repr(C, packed)]
+#[cfg(target_pointer_width = "32")]
+struct GdtLocator {
+    pub Length: u16,
+    //pub BaseAddress: u64,
+    pub BaseAddress: *const Gdt,
+}
+
+impl Gdt {
+    pub fn create32BitFlat() -> Self {
+
+        // Figure 3-8. Segment Descriptor
+        // The +32 is to just make it easier to match against the figure for the upper half
+        let dataSection = 
+            0xFFFF << 0      // Limit 15:0
+            | 0x0 << 16      // Base 15:0
+            | 0x0 << (32+0)  // Base 23:16
+            | 0x2 << (32+8)  // Type - W Read/Write
+            | 0x1 << (32+12) // S Field - Appliation Descriptor (Code or Data)
+            | 0x0 << (32+13) // DPL - Ring 0
+            | 0x1 << (32+15) // Present
+            | 0xF << (32+16) // Limit 19:16
+            | 0x0 << (32+20) // Free for use by system software - we don't need it
+            | 0x0 << (32+21) // L - Only 32-bit for now
+            | 0x1 << (32+22) // D/B - Don't think we need this
+            | 0x1 << (32+23) // G - Granularity, use 4KB units
+            | 0x0 << (32+24) // Base 31:24
+            ;
+
+        let codeSection = dataSection 
+            | 0x8 << (32+8); // Type - Execute; plus Read as we're inheriting from dataSection
+
+        Self { 
+            nullSection: 0, 
+            codeSection: codeSection,
+            dataSection: dataSection,
+        }
+    }
+
+    pub fn load(&self) {
+        let size :u16 = core::mem::size_of::<Gdt>().try_into().unwrap();
+        let locator = GdtLocator {
+            // "Because segment descriptors are always 8 bytes long, the GDT limit should 
+            // always be one less than an integral multiple of eight (that is, 8N – 1)"
+            Length: size - 1,
+            BaseAddress: self,
+        };
+
+        unsafe {
+            asm!(
+                "lgdt [{}]",
+                in(reg) &locator,
+                options(nostack, preserves_flags),
+            );
+        }
+    }
+
+    // Only to be called from Real mode
+    #[cfg(target_pointer_width = "32")]
+    pub unsafe fn enterUnrealMode(&self) {
+
+        self.load();
+
+        unsafe {
+            asm!(
+                "push ds",
+                "push ss",
+                "mov eax, cr0",
+                "mov ebx, eax", // Unmodified CR0 we'll reset to later
+                "or eax, 0x1",  // Set protected mode bit
+                "mov cr0, eax", // Enter protected mode
+                "mov ax, 0x10", // Index 2 (the data segment) into the GDT
+                "mov ds, ax",
+                "mov ss, ax",
+                "mov cr0, ebx", // Back to how it was; real mode
+                "pop ss",
+                "pop ds",
+                out("eax") _,
+                out("ebx") _,
+            );
+        }
+    }
+}
+
+
+
+
+
+
+
+
 
 #[repr(C, packed)]
 struct OurGdt {
@@ -25,13 +127,8 @@ pub struct GDTR {
     Base: u64,
 }
 
-// 3.5.1 Segment Descriptor Tables
-#[repr(C, packed)]
-pub struct GDT {
-    nullSection: u64,
-    codeSection: u64,
-    dataSection: u64,
-}
+
+
 
 pub unsafe fn Setup64BitGDT(baseAddress: u64, cantUseAbove: usize) {
 
@@ -75,9 +172,9 @@ pub unsafe fn Setup64BitGDT(baseAddress: u64, cantUseAbove: usize) {
     );
 }
 
-impl GDT {
+impl Gdt {
     pub fn new() -> Self {
-        GDT { 
+        Gdt { 
             nullSection: 0, 
             codeSection: 
                 1 << (32 + 21)      /* Long Mode - 64bit */ 
@@ -102,9 +199,9 @@ impl GDTR {
         GDTR { Limit: 0, Base: 0 }
     }
 
-    pub unsafe fn install(&mut self, gdt: GDT) {
+    pub unsafe fn install(&mut self, gdt: Gdt) {
         self.Base = &gdt as *const _ as u64;
-        self.Limit = size_of::<GDT>().try_into().unwrap();
+        self.Limit = size_of::<Gdt>().try_into().unwrap();
 
         asm!(
             "lgdt [eax]",
@@ -119,13 +216,16 @@ struct GdtrInternal {
     BaseAddress: u64,
 }
 
-pub struct Gdtr {
+#[cfg(target_pointer_width = "64")]
+pub struct GdtLocator {
     pub BaseAddress: u64,
     pub Length: u16,
 }
 
+
+
 #[cfg(target_pointer_width = "64")]
-pub fn GetGdtr() -> Gdtr {
+pub fn GetGdtr() -> GdtLocator {
     let gdtr = GdtrInternal{BaseAddress: 0,Length: 0};
 
     unsafe {
@@ -140,7 +240,7 @@ pub fn GetGdtr() -> Gdtr {
     let limit = gdtr.Length;
     let base = gdtr.BaseAddress;
 
-    Gdtr{
+    GdtLocator{
         BaseAddress :base,
         Length:limit,
     }
