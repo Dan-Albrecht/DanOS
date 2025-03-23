9375f51cc346d1b86b4bfac1166bda869e2f6681
Dan Albrecht <Dan-Albrecht@users.noreply.github.com>
2025-03-23 15:08:32 -0700

    Detect if the img isn't mounted and we're just writing to nowhere and testing an old build :(

--- build/iterateInternal.ps1
+++ build/iterateInternal.ps1
@@ -21,25 +21,18 @@ try {
     else {
         $targetType = "release"
     }
-    
-    TimeCommand { ../stage2_rust/build.ps1 -loadTarget $STAGE_2_LOAD_TARGET -debug $debug } -message 'Stage 2'
-    $stage2Path = "../stage2_rust/target/i386-unknown-none/$targetType/stage2_rust.bin"
-
-    $stage2Bytes = Get-Content $stage2Path -Raw -AsByteStream
-    $stage2Item = Get-ChildItem $stage2Path
-    $stage2Sectors = [Math]::Ceiling($stage2Bytes.Length / 512)
-    $stage2Padding = $stage2Sectors * 512 - $stage2Bytes.Length
-    Write-Host "Stage2 is @ $stage2Path size is $($stage2Item.Length) written @ $($stage2Item.LastWriteTime) and will need $stage2Padding padding"
-
-    Write-Host "Stage 1 @ 0x7C00 (must be 1 sector)"
-    Write-Host "Stage 2 @ 0x$(([int]$STAGE_2_LOAD_TARGET).ToString("X")) (for 0x$(([int]$stage2Sectors).ToString("X")) sectors)"
-    Write-Host "This is a total of 0x$(([int]$stage2Sectors).ToString("X")) sectors to load from disk to address 0x$(([int]$STAGE_2_LOAD_TARGET).ToString("X"))."
 
     TimeCommand { ../stage1/build.ps1 -sectorsToLoad $stage2Sectors -addressToLoadTo $STAGE_2_LOAD_TARGET } -message 'Stage 1'
     $stage1Path = "../stage1/bootloaderStage1.bin"
-
+    
+    TimeCommand { ../stage2_rust/build.ps1 -loadTarget $STAGE_2_LOAD_TARGET -debug $debug } -message 'Stage 2'
+    $stage2Path = "../stage2_rust/target/i386-unknown-none/$targetType/stage2_rust.bin"
+    
     TimeCommand { ../kernel/buildKernel.ps1 -debug $debug } -message 'Kernel32'
+    $stage3Path = "../kernel/target/i686-unknown-none/$targetType/kernel.bin"
+    
     TimeCommand { ../kernel64/buildKernel.ps1 -debug $debug } -message 'Kernel64'
+    $stage4Path = "../kernel64/target/x86_64-unknown-none/$targetType/kernel64"
 
     $stage1Bytes = Get-Content $stage1Path -Raw -AsByteStream
     $stage1Item = Get-ChildItem $stage1Path
@@ -54,15 +47,25 @@ try {
     if ($stage1Bytes.Length + $stage2Bytes.Length -gt $BOOTLOADER_MAX_SIZE) {
         Write-Error "Bootloaders are too big"
     }
-
+    
+    $stage2Bytes = Get-Content $stage2Path -Raw -AsByteStream
+    $stage2Item = Get-ChildItem $stage2Path
+    $stage2Sectors = [Math]::Ceiling($stage2Bytes.Length / 512)
+    $stage2Padding = $stage2Sectors * 512 - $stage2Bytes.Length
+    
+    # BUGBUG: This name is confusing
     if (![System.IO.File]::Exists("empty.img")) {
         Write-Error "empty.img doesn't exist. You'll need to create it by hand. Follow the README.md."
     }
 
-    # BUGBUG: Ensure empty.img is mounted
+    # BUGUBG: Probably a better way to check this (that the img is mounted), but we're always hand backing this file into the image, so good enough for now
+    if (![System.IO.File]::Exists("/mnt/danOS/hi.txt")) {
+        Write-Error "It appears empty.img isn't mounted. You'll need to mount it to /mnt/danOS."
+    }
+
     TimeCommand {
-        cp ../kernel/target/i686-unknown-none/$targetType/kernel.bin /mnt/danOS/kernel.bin
-        cp ../kernel64/target/x86_64-unknown-none/$targetType/kernel64 /mnt/danOS/kernel64.elf
+        cp $stage3Path /mnt/danOS/kernel.bin
+        cp $stage4Path /mnt/danOS/kernel64.elf
         sync
     } -message 'Copy kernels'
 
@@ -80,6 +83,15 @@ try {
 
         $fs.Close()
     } -message 'Assemble image'
+
+    Write-Host "Stage 1 @ 0x7C00 (must be 1 sector)"
+    Write-Host "$($stage1Path)"
+    Write-Host "Stage 2 @ 0x$(([int]$STAGE_2_LOAD_TARGET).ToString("X")) (for 0x$(([int]$stage2Sectors).ToString("X")) sectors)"
+    Write-Host "Stage2 is @ $stage2Path size is $($stage2Item.Length) written @ $($stage2Item.LastWriteTime) and will need $stage2Padding padding"
+    Write-Host "This is a total of 0x$(([int]$stage2Sectors).ToString("X")) sectors to load from disk to address 0x$(([int]$STAGE_2_LOAD_TARGET).ToString("X"))."
+    Write-Host "$($stage2Path)"
+    Write-Host "$($stage3Path)"
+    Write-Host "$($stage4Path)"
 }
 finally {
     $PSNativeCommandUseErrorActionPreference = $oldErrorState
